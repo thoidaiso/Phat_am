73bb4a9c61525cdfa535cf436efecd2a98b84870
Chanh Le <giaosudau@gmail.com>
2013-12-15 01:39:31 +0700

    PullLoadView run

--- src/com/example/showlistcontent/VideoMainActivity.java
+++ src/com/example/showlistcontent/VideoMainActivity.java
@@ -2,48 +2,76 @@ package com.example.showlistcontent;
 
 import java.util.ArrayList;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
 
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 import com.example.listview.ListVideoAdapter;
-import com.example.listview.Model_Video;
+import com.example.listview.VideoItem;
 import com.example.phat_am.R;
 
 public class VideoMainActivity extends SherlockFragmentActivity {
 
 	ViewPager pager;
 	static int NUM_PAGES = 3;
-	public ArrayList<Model_Video> list_model = new ArrayList<Model_Video>();
+	public ArrayList<VideoItem> list_model = new ArrayList<VideoItem>();
 	ListView list;
 	ListVideoAdapter adapter;
-	int type;// top,new,random video
+	String type;// top,new,random video
+	int currentPage;
+
+	public String getUrl() {
+		return this.type;
+	}
 
 	@Override
 	protected void onCreate(Bundle arg0) {
 		// TODO Auto-generated method stub
 		super.onCreate(arg0);
-		Bundle bun = getIntent().getExtras();
-		if (bun != null)
-			type = bun.getInt("type");
-		Log.v("type==========", type + "");
+
+		Intent intent = getIntent();
+		type = intent.getStringExtra("type");
+		String isOnline = intent.getStringExtra("isOnline");
 		setContentView(R.layout.viewpager_titlestrip_activity);
 		pager = (ViewPager) findViewById(R.id.pager);
-		FragmentPagerAdapter adapter = new FragmentPagerAdapter(
-				getSupportFragmentManager());
+		final FragmentPagerAdapter adapter = new FragmentPagerAdapter(
+				getSupportFragmentManager(), this);
 		pager.setAdapter(adapter);
-		// pager.setCurrentItem(page);
+
+		pager.setOnPageChangeListener(new OnPageChangeListener() {
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageSelected(int position) {
+				pager.setCurrentItem(position, true);
+				adapter.getItem(pager.getCurrentItem()).getView();
+
+			}
+
+		});
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 	}
 
 	@Override
@@ -62,21 +90,35 @@ public class VideoMainActivity extends SherlockFragmentActivity {
 
 	public class FragmentPagerAdapter extends
 			android.support.v4.app.FragmentPagerAdapter {
+		VideoMainActivity mActivity;
+		private ArrayList<Fragment> fragments;
 
-		public FragmentPagerAdapter(FragmentManager fm) {
+		public FragmentPagerAdapter(FragmentManager fm, VideoMainActivity ac) {
 			super(fm);
-			// TODO Auto-generated constructor stub
+			mActivity = ac;
 		}
 
 		@Override
-		public Fragment getItem(int postition) {
-			// TODO Auto-generated method stub
-			// if (postition == 1)
-			Log.v("Position ========", Integer.toString(postition));
-			{
-				Fragment fr = new loadmore_video_fragment("topvideos", "rating");
-				return fr;
+		public int getItemPosition(Object object) {
+			return pager.getCurrentItem();
+		}
+		
+		@Override
+		public Fragment getItem(int position) {
+			Fragment fr;
+			Log.v("Position xx", Integer.toString(position));
+			if (position == 0) {
+				fr = new loadmore_video_fragment(mActivity.getUrl(),
+						"site_views");
+			} else if (position == 1) {
+				fr = new loadmore_video_fragment(mActivity.getUrl(), "added");
+			} else {
+				fr = new loadmore_video_fragment(mActivity.getUrl(),
+						"video_title");
+
 			}
+			return fr;
+
 		}
 
 		@Override
@@ -97,26 +139,22 @@ public class VideoMainActivity extends SherlockFragmentActivity {
 				return getString(R.string.type_video_title);
 			}
 			return ("");
-			// return "Section" + (position +1);
-
 		}
 
 	}
 
-//	@SuppressLint("ValidFragment")
-//	public class ViewPagerAdapter extends Fragment {
-//		@Override
-//		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//				Bundle savedInstanceState) {
-//			// TODO Auto-generated method stub
-//			View rootView = inflater.inflate(R.layout.list, container, false);
-//			Bundle bun = getArguments();
-//			int value = bun.getInt("key");
-//			CharSequence sequence = getString(R.string.string) + value;
-//			((TextView) rootView.findViewById(R.id.text)).setText(sequence);
-//			return rootView;
-//		}
-//
-//	}
+	/*
+	 * @SuppressLint("ValidFragment") public class ViewPagerAdapter extends
+	 * Fragment {
+	 * 
+	 * @Override public View onCreateView(LayoutInflater inflater, ViewGroup
+	 * container, Bundle savedInstanceState) { // TODO Auto-generated method
+	 * stub View rootView = inflater.inflate(R.layout.list, container, false);
+	 * Bundle bun = getArguments(); int value = bun.getInt("key"); CharSequence
+	 * sequence = getString(R.string.string) + value; ((TextView)
+	 * rootView.findViewById(R.id.text)).setText(sequence); return rootView; }
+	 * 
+	 * }
+	 */
 
 }
--- src/com/example/showlistcontent/loadmore_video_fragment.java
+++ src/com/example/showlistcontent/loadmore_video_fragment.java
@@ -1,7 +1,10 @@
 package com.example.showlistcontent;
 
-
 import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -9,190 +12,274 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
-import com.actionbarsherlock.app.SherlockFragment;
 import com.costum.android.widget.PullAndLoadListView;
 import com.costum.android.widget.PullAndLoadListView.OnLoadMoreListener;
 import com.costum.android.widget.PullToRefreshListView.OnRefreshListener;
+import com.devspark.progressfragment.SherlockProgressFragment;
 import com.example.listview.ListVideoAdapter;
 import com.example.listview.VideoItem;
 import com.example.phat_am.R;
+import com.example.utils.Helper;
+import com.example.utils.RestClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 @SuppressLint("ValidFragment")
-public class loadmore_video_fragment extends SherlockFragment{
+public class loadmore_video_fragment extends SherlockProgressFragment {
 
 	public ArrayList<VideoItem> list_model = new ArrayList<VideoItem>();
-	PullAndLoadListView listview;
+	ListView listview;
+	private View mContentView;
 	ListVideoAdapter adapter;
-	static String type;//top,random,new
-	static String order; //order by date,title,rating
-	
+	String type;// top,random,new
+	String order; // order by date,title,rating
+	private Runnable mShowContentRunnable = new Runnable() {
+
+		@Override
+		public void run() {
+			setContentShown(true);
+		}
+
+	};
+	private Handler mHandler;
+
+	public loadmore_video_fragment() {
+		this.type = "topvideos";
+		this.order = "added";
+	}
+
 	public loadmore_video_fragment(String type, String order) {
 		// TODO Auto-generated constructor stub
-		loadmore_video_fragment.type = type;
-		loadmore_video_fragment.order = order;
-		Log.v("video fragment type", ""+type);
-		Log.v("order", ""+order);
+		this.type = type;
+		this.order = order;
+		Log.v("type", "" + type);
+		Log.v("order", "" + order);
 	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		setContentView(mContentView);
+		setEmptyText(R.string.empty);
+		obtainData(this.type, this.order);
+	}
+
+	private void obtainData(String type, String order) {
+		setContentShown(false);
+		RestClient.get(type, null, new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONObject result) {
+				JSONArray jArray;
+				try {
+					jArray = result.getJSONArray("videos");
+					for (int i = 0; i < jArray.length(); i++) {
+						JSONObject line_object = jArray.getJSONObject(i);
+						// int id = line_object.getInt("id");
+						String uniq_id = line_object.getString("uniq_id");
+						String artist = line_object.getString("artist");
+						String video_title = line_object
+								.getString("video_title");
+						String description = line_object
+								.getString("description");
+						String yt_thumb = line_object.getString("yt_thumb");
+						int site_views = line_object.getInt("site_views");
+						// String mp3 = line_object.getString("audio");
+						String yt_id = line_object.getString("yt_id");
+						VideoItem item = new VideoItem(uniq_id, artist,
+								video_title, description, yt_id, yt_thumb,
+								site_views);
+						list_model.add(item);
+						adapter.notifyDataSetChanged();
+
+					}
+
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		mHandler = new Handler();
+		mHandler.postDelayed(mShowContentRunnable, 500);
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View rootView = inflater.inflate(R.layout.loadmore_list, container, false);
+		mContentView = inflater.inflate(R.layout.loadmore_list, container,
+				false);
 		/*
-		String[] list_video = getResources().getStringArray(R.array.detail_video_array);
-		String[] list_author = getResources().getStringArray(R.array.detail_video_author_array);
-		*/
-		Bitmap bm = BitmapFactory.decodeResource(this.getResources(), R.drawable.image);
-//		list_model.clear();
-		/*for(int i=0; i<8; i++)
-		{
-			list_model.add(new Model_Video(bm, list_video[i], list_author[i]));
-		}*/
-		listview = (PullAndLoadListView)rootView.findViewById(R.id.list_view);
+		 * String[] list_video =
+		 * getResources().getStringArray(R.array.detail_video_array); String[]
+		 * list_author =
+		 * getResources().getStringArray(R.array.detail_video_author_array);
+		 */
+		// Bitmap bm = BitmapFactory.decodeResource(this.getResources(),
+		// R.drawable.image);
+		// list_model.clear();
+		/*
+		 * for(int i=0; i<8; i++) { list_model.add(new Model_Video(bm,
+		 * list_video[i], list_author[i])); }
+		 */
+
+		listview = (PullAndLoadListView) mContentView
+				.findViewById(R.id.load_more_list_view);
 		adapter = new ListVideoAdapter(getSherlockActivity(), list_model);
 		listview.setAdapter(adapter);
 		listview.setOnItemClickListener(OnItemClick);
-//		Helper.getListViewSize(listview);
 		// Set a listener to be invoked when the list should be refreshed.
-		listview.setOnRefreshListener(new OnRefreshListener() {
-
-					@Override
-					public void onRefresh() {
-						// Do work to refresh the list here.
-						new PullToRefreshDataTask().execute();
-					}
-				});
+//		listview.setOnRefreshListener(new OnRefreshListener() {
+//
+//			@Override
+//			public void onRefresh() {
+//				// Do work to refresh the list here.
+//				new PullToRefreshDataTask().execute();
+//			}
+//		});
 
 		// set a listener to be invoked when the list reaches the end
-		listview.setOnLoadMoreListener(new OnLoadMoreListener() {
-					
-					@Override
-					public void onLoadMore() {
-						// Do the work to load more items at the end of list
-						// here
-						new LoadMoreDataTask().execute();
-					}
-				});
-		return rootView;
+//		listview.setOnLoadMoreListener(new OnLoadMoreListener() {
+//
+//			@Override
+//			public void onLoadMore() {
+//				// Do the work to load more items at the end of list
+//				// here
+//				new LoadMoreDataTask().execute();
+//			}
+//		});
+		return super.onCreateView(inflater, container, savedInstanceState);
 	}
-	
+
 	private OnItemClickListener OnItemClick = new OnItemClickListener() {
 
 		@Override
 		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 				long arg3) {
 			// TODO Auto-generated method stub
-//			Log.v("u click",list_model.get(arg2).getName().toString());
-			Intent i = new Intent(getSherlockActivity(), VideoInfoActivity.class);
-			/*i.putExtra("link", list_model.get(arg2).getName().toString());
-            startActivity(i);*/
+			// Log.v("u click",list_model.get(arg2).getName().toString());
+			Intent i = new Intent(getSherlockActivity(),
+					VideoInfoActivity.class);
+			/*
+			 * i.putExtra("link", list_model.get(arg2).getName().toString());
+			 * startActivity(i);
+			 */
 		}
 	};
-	
-	private class LoadMoreDataTask extends AsyncTask<Void, Void, Void> {
 
-		@Override
-		protected Void doInBackground(Void... params) {
-
-			if (isCancelled()) {
-				return null;
-			}
-			// Simulates a background task
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-			}
-			//Put code to download list video data here, and add to list_model
-			String[] list_video = getResources().getStringArray(R.array.detail_video_array);
-			String[] list_author = getResources().getStringArray(R.array.detail_video_author_array);
-			
-			Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
-			
-			/*for(int i=0; i<8; i++)
-			{
-				list_model.add(new Model_Video(bm, list_video[i], list_author[i]));
-			}*/
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Void result) {
-			//Todo: Delete 2 line after finish all
-			Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
-//			list_model.add(new Model_Video(bm, "Loadmore", "Loadmore"));
-
-			// We need notify the adapter that the data have been changed
-			adapter.notifyDataSetChanged();
-
-			// Call onLoadMoreComplete when the LoadMore task, has finished
-			listview.onLoadMoreComplete();
-			return ;
-//			super.onPostExecute(result);
-		}
-
-		@Override
-		protected void onCancelled() {
-			// Notify the loading more operation has finished
-			listview.onLoadMoreComplete();
-		}
-	}
-
-	private class PullToRefreshDataTask extends AsyncTask<Void, Void, Void> {
-
-		@Override
-		protected Void doInBackground(Void... params) {
-
-			if (isCancelled()) {
-				return null;
-			}
-
-			// Simulates a background task
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-			}
-			//Put code to download list video data here, and add to list_model
-			String[] list_video = getResources().getStringArray(R.array.detail_video_array);
-			String[] list_author = getResources().getStringArray(R.array.detail_video_author_array);
-			
-			Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
-			/*
-			for(int i=0; i<8; i++)
-			{
-				list_model.add(0,new Model_Video(bm, list_video[i], list_author[i]));
-			}*/
-
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Void result) {
-			//Todo: Delete 2 line after finish all
-			Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
-//			list_model.add(0, new Model_Video(bm, "Add after pull to reffress", "Loadmore"));
-
-			// We need notify the adapter that the data have been changed
-			adapter.notifyDataSetChanged();
-
-			// Call onLoadMoreComplete when the LoadMore task, has finished
-			listview.onRefreshComplete();
+//	private class LoadMoreDataTask extends AsyncTask<Void, Void, Void> {
+//
+//		@Override
+//		protected Void doInBackground(Void... params) {
+//
+//			if (isCancelled()) {
+//				return null;
+//			}
+//			// Simulates a background task
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//			}
+//			// Put code to download list video data here, and add to list_model
+//			/*
+//			 * String[] list_video =
+//			 * getResources().getStringArray(R.array.detail_video_array);
+//			 * String[] list_author =
+//			 * getResources().getStringArray(R.array.detail_video_author_array);
+//			 */
+//			Bitmap bm = BitmapFactory.decodeResource(getResources(),
+//					R.drawable.image);
+//
+//			/*
+//			 * for(int i=0; i<8; i++) { list_model.add(new Model_Video(bm,
+//			 * list_video[i], list_author[i])); }
+//			 */
+//			return null;
+//		}
+//
+//		@Override
+//		protected void onPostExecute(Void result) {
+//			// Todo: Delete 2 line after finish all
+//			Bitmap bm = BitmapFactory.decodeResource(getResources(),
+//					R.drawable.image);
+//			// list_model.add(new Model_Video(bm, "Loadmore", "Loadmore"));
+//
+//			// We need notify the adapter that the data have been changed
+//			adapter.notifyDataSetChanged();
+//
+//			// Call onLoadMoreComplete when the LoadMore task, has finished
+//			listview.onLoadMoreComplete();
 //			return;
-			super.onPostExecute(result);
-		}
+//			// super.onPostExecute(result);
+//		}
+//
+//		@Override
+//		protected void onCancelled() {
+//			// Notify the loading more operation has finished
+//			listview.onLoadMoreComplete();
+//		}
+//	}
+//
+//	private class PullToRefreshDataTask extends AsyncTask<Void, Void, Void> {
+//
+//		@Override
+//		protected Void doInBackground(Void... params) {
+//
+//			if (isCancelled()) {
+//				return null;
+//			}
+//
+//			// Simulates a background task
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//			}
+//			// Put code to download list video data here, and add to list_model
+//			String[] list_video = getResources().getStringArray(
+//					R.array.detail_video_array);
+//			String[] list_author = getResources().getStringArray(
+//					R.array.detail_video_author_array);
+//
+//			Bitmap bm = BitmapFactory.decodeResource(getResources(),
+//					R.drawable.image);
+//			/*
+//			 * for(int i=0; i<8; i++) { list_model.add(0,new Model_Video(bm,
+//			 * list_video[i], list_author[i])); }
+//			 */
+//
+//			return null;
+//		}
+//
+//		@Override
+//		protected void onPostExecute(Void result) {
+//			// Todo: Delete 2 line after finish all
+//			Bitmap bm = BitmapFactory.decodeResource(getResources(),
+//					R.drawable.image);
+//			// list_model.add(0, new Model_Video(bm,
+//			// "Add after pull to reffress", "Loadmore"));
+//
+//			// We need notify the adapter that the data have been changed
+//			adapter.notifyDataSetChanged();
+//
+//			// Call onLoadMoreComplete when the LoadMore task, has finished
+//			listview.onRefreshComplete();
+//			// return;
+//			super.onPostExecute(result);
+//		}
+//
+//		@Override
+//		protected void onCancelled() {
+//			// Notify the loading more operation has finished
+//			listview.onLoadMoreComplete();
+//		}
+//	}
 
-		@Override
-		protected void onCancelled() {
-			// Notify the loading more operation has finished
-			listview.onLoadMoreComplete();
-		}
-	}
-	
-		
 }
